c00c6010f8dc9c6a73e9dfed0c36475027446b74
sergigallardo <sgallardo@institutmvm.cat>
2021-12-09 12:55:50 +0100

    Update: UF1-A11-Ex2 finalitzat

--- UF1-A11-Ex2.py
+++ UF1-A11-Ex2.py
@@ -1,6 +1,6 @@
 def main():
     x = 0
-    llista = [x + 1 for x in range(30) if x % 2 != 0]
+    llista = [x  for x in range(1000) if x % 8 == 0]
     print(llista)
 if __name__ == '__main__':
     main()
