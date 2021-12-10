e2de19f7a57f7187c64351d9ab1ea497a89882f3
sergigallardo <sgallardo@institutmvm.cat>
2021-12-10 12:21:53 +0100

    Update: UF1-A11-Ex4 updated

--- UF1-A11-Ex5.py
+++ UF1-A11-Ex5.py
@@ -1,6 +1,7 @@
 def main():
     state = "Practica els problemes de list comprehensions per a ser més Pythonic!"
     consonants = [ i for i in state if i not in 'AaEeIiOoUu']
+    #Elimina las letras vocales de la frase de la variable state
     print(consonants)
 
 if __name__ == '__main__':
