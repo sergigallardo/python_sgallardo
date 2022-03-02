cc54dc77e089202265628c842c8b5dcbb6961db8
sergigallardo <60615701+sergigallardo@users.noreply.github.com>
2022-03-02 13:48:53 +0100

    Update functions.py

--- UF3/M03. UF3. Activitats/Ex4/functions.py
+++ UF3/M03. UF3. Activitats/Ex4/functions.py
@@ -5,7 +5,19 @@ def inputs():
         name = input("Introdueix el teu nom:")
         surname = input("Introdueix el teu cognom:")
         age = input("Introdueix la teva edat:")
-        tecnology = input("Introdueix la tecnologia en la que estas interessat/da:")
+        selection = int(input("Introdueix la tecnologia en la que estas interessat/da:\n---------------------------------------------\n1.Robotica\n2.Inteligencia artificial\n3.Nanotecnologia\n4.Informatica cuantica\n5.Bioinformática\n-------------------------------------------\nSeleccció:"))
+        match selection:
+            case 1:
+                tecnology = "Robotica"
+            case 2:
+                tecnology = "Inteligencia articial"
+            case 3:
+                tecnology = "Nanotecnologia"
+            case 4:
+                tecnology = "Informatica cuantica"
+            case 5:
+                tecnology = "Bioinformatica"
+
         id = (name[:2] + surname[:2] + age)
 
         with open('register.csv', 'a') as csvfile:
